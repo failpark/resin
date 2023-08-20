46d09891ab96d3301aa6d4e5f7057089d4e24396
Phedias <daniel.borgs@noxa.de>
2023-08-20 11:36:01 +0200

    feat: handle ctrl+c interrupt

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,21 @@ mod inputs;
 mod utils;
 
 fn main() {
+
+	ctrlc::set_handler(move || {
+		let term = console::Term::stderr();
+		let _ = term.show_cursor();
+	}).expect("Error setting ctrl+c handler");
+
 	let args = cli::setup().get_matches();
 	let config = conf::read().expect("Failed to read from configuration file");
-	let inputs = inputs::get_inputs(&config).expect("Failed to get scope");
-	git::commit_changes(&config, &args, &inputs).expect("Failed to commit changes");
+	let inputs = inputs::get_inputs(&config);
+	if let Ok(inputs) = inputs {
+		git::commit_changes(&config, &args, &inputs).expect("Failed to commit changes");
+	} else {
+		let term = console::Term::stderr();
+		let _ = term.show_cursor();
+		std::process::exit(1);
+		// inputs.expect("Failed to get scope");
+	}
 }
